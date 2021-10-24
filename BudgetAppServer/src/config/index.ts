1b62c3a5f598a6fe267e8a799c8c95622ea75560
SujithGunasekaran <mydevelopstar@gmail.com>
2021-10-24 21:34:20 +0530

    JWT package installed

--- BudgetAppServer/src/config/index.ts
+++ BudgetAppServer/src/config/index.ts
@@ -5,5 +5,6 @@ dotEnv.config();
 export const config = {
     PORT: process.env.PORT,
     MONGODBURI: process.env.MONGODBURI,
+    JWTSECRET: process.env.JWTSECRET,
     LOCAL_URL: process.env.LOCAL_URL
 }
